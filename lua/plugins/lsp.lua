cd9aaf73d20de2379ad1fd7e959032266c67e39d
Vladyslav49 <Timofeevvladyslav49@gmail.com>
2024-09-30 20:30:39 +0100

    add live-rename plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,73 +1,72 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.typos_lsp.setup {}
+            lspconfig.typos_lsp.setup {}
 
-			-- python
-			lspconfig.ruff.setup {}
-			lspconfig.pyright.setup {
-				settings = {
-					python = {
-						analysis = {
-							typeCheckingMode = "off",
-						},
-					},
-				},
-			}
-			lspconfig.pylsp.setup {
-				settings = {
-					pylsp = {
-						plugins = {
-							pylsp_mypy = {
-								enabled = true,
-								overrides = { "--enable-incomplete-feature", "NewGenericSyntax", true },
-							},
-						},
-					},
-				},
-			}
+            -- python
+            lspconfig.ruff.setup {}
+            lspconfig.pyright.setup {
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "off",
+                        },
+                    },
+                },
+            }
+            lspconfig.pylsp.setup {
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            pylsp_mypy = {
+                                enabled = true,
+                                overrides = { "--enable-incomplete-feature", "NewGenericSyntax", true },
+                            },
+                        },
+                    },
+                },
+            }
 
-			-- go
-			lspconfig.gopls.setup {}
+            -- go
+            lspconfig.gopls.setup {}
 
-			-- lua
-			lspconfig.lua_ls.setup {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			}
+            -- lua
+            lspconfig.lua_ls.setup {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            }
 
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename Symbol" })
-					vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "<Leader>lf", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
-			})
-		end
-	}
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<Leader>lf", function()
+                        vim.lsp.buf.format({ async = true })
+                    end, opts)
+                end,
+            })
+        end
+    }
 }
