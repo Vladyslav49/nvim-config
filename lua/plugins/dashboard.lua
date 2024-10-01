72eb647eeb168ffc4d5be9d2d3f9206082694edd
Vladyslav49 <Timofeevvladyslav49@gmail.com>
2024-10-01 18:25:31 +0100

    add dashboard plugin

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,41 @@
+return {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("dashboard").setup({
+            theme = "hyper",
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    {
+                        icon = "",
+                        icon_hl = "@variable",
+                        desc = " Files",
+                        group = "Label",
+                        action = "Telescope smart_open",
+                        key = "f",
+                    },
+                    {
+                        icon = "",
+                        icon_hl = "@variable",
+                        desc = " Grep",
+                        group = "Label",
+                        action = "Telescope live_grep",
+                        key = "g",
+                    },
+                    {
+                        icon = "",
+                        icon_hl = "@variable",
+                        desc = " Tree",
+                        group = "Label",
+                        action = "Neotree toggle left reveal",
+                        key = "t",
+                    },
+                },
+            },
+        })
+    end
+}
