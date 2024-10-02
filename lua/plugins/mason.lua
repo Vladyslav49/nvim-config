c6e2c1648dc253f5dacedea915e601162419f9f1
Vladyslav49 <Timofeevvladyslav49@gmail.com>
2024-10-02 21:16:24 +0100

    add gopls to mason ensure_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,24 +1,26 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"typos_lsp",
-					-- python
-					"ruff",
-					"pyright",
-					"pylsp",
-					-- lua
-					"lua_ls"
-				}
-			})
-		end
-	}
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "typos_lsp",
+                    -- python
+                    "ruff",
+                    "pyright",
+                    "pylsp",
+                    -- go
+                    "gopls",
+                    -- lua
+                    "lua_ls",
+                }
+            })
+        end
+    }
 }
