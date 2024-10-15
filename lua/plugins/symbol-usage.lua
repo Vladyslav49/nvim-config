a9f35cbf977eb97a98916b3e994ef7b2ca8a0eb3
Vladyslav49 <Timofeevvladyslav49@gmail.com>
2024-10-15 19:24:07 +0100

    fix symbol-usage setup

--- lua/plugins/symbol-usage.lua
+++ lua/plugins/symbol-usage.lua
@@ -2,6 +2,8 @@ return {
     "Wansmer/symbol-usage.nvim",
     event = "LspAttach",
     config = function()
-        require("symbol-usage").setup({})
+        require("symbol-usage").setup({
+            disable = { lsp = { "pylsp", "basedpyright" } }
+        })
     end
 }
