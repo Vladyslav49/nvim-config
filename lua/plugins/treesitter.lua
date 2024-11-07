22c550be6c1de5b07b956077ee66fba5c1eae1fb
Vladyslav49 <Timofeevvladyslav49@gmail.com>
2024-11-07 13:11:32 +0000

    add hyprlang to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,14 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "python", "go" },
-				highlight = { enable = true }
-			})
-		end
-	}
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "python", "go", "hyprlang" },
+                highlight = { enable = true }
+            })
+            vim.filetype.add({
+                pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+            })
+        end
+    }
 }
